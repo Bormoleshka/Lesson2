a4ae2a363033114230c6cce02565d52bd7c31657
Алексей Бормотов <bormoleshka@gmail.com>
2021-10-28 22:18:01 +0300

    add random

---
+++ randomray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] a=new int[10];
+Random rnd=new Random();//создали класс для случайных чисел
+Console.WriteLine(rnd.Next(0,100));
+for(int i=0;i<10;++i)
+{
+    a[i]=rnd.Next(0,100);
+}
+for(int i=0;i<10;i++)
+{
+    Console.Write(a[i]+" ");
+
+}
